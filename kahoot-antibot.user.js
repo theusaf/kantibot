edd2e92aec233f4713f0ae8046cb1efd16756a25
theusaf <theusafyt@gmail.com>
2020-11-08 08:54:33 -0800

    2.8.10

--- kahoot-antibot.user.js
+++ kahoot-antibot.user.js
@@ -562,20 +562,20 @@ window.page.onload = ()=>{
         if(c && !locked){
           if(!!(+windw.specialData.config.ddos) && (+c.innerHTML - oldamount) > (+windw.specialData.config.ddos/3)){
             locked = true;
+            const oldpin = pin;
             // LOCK THE GAME!
             setTimeout(()=>{
               e.webSocket.send(JSON.stringify([{
                 channel: "/service/player",
                 clientId,
                 data: {
-                  gameid: pin,
+                  gameid: oldpin,
                   type: "lock"
                 },
                 ext: {},
                 id: ++messageId
               }]));
             },1e3);
-            const oldpin = pin;
             console.log("[ANTIBOT] - Detected bot spam. Locking game for 1 minute.");
             setTimeout(()=>{
               locked = false;
