40a246a90e2e3dc16ea110754288cf09b8924c1e
theusaf <theusafyt@gmail.com>
2021-11-18 16:26:53 -0800

    Update logo

--- kahoot-antibot.user.js
+++ kahoot-antibot.user.js
@@ -259,7 +259,7 @@ const kantibotProgramCode = () => {
   // create watermark
   const UITemplate = document.createElement("template");
   UITemplate.innerHTML = `<div id="antibotwtr">
-    <p>v3.2.0 ©theusaf</p>
+    <p>v3.2.1 ©theusaf</p>
     <p id="antibot-killcount">0</p>
     <details>
       <summary>config</summary>
